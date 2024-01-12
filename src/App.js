d8667e4117a95c1469a62000390c67e86feb1dd6
ronikapav <ronikapav@gmail.com>
2024-01-12 21:22:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { data } from './data';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [attractions, setAttractions] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [landmark, setLandmark] = useState(data);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => 
+        prevIndex === landmark.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+    return () => clearInterval(interval);
+  }, 
+  [landmark.length]);
+
+  function delAttractions(id) {
+    let newAttractions = landmark.filter(attraction => attraction.id !== id);
+    setLandmark(newAttractions);
+
+    if (attractions > 0) setAttractions(attractions => attractions - 1);
+    else setAttractions(0);
+  }
+
+  const previousAttractions = () => {
+    setAttractions(attractions =>{
+      attractions--;
+      if(attractions<0) {
+        return landmark.length-1;
+      }
+      return attractions;
+    })
+  }
+
+  const nextAttractions = () => {
+    setAttractions(attractions =>{
+      attractions++;
+      if(attractions>landmark.length-1) {
+        attractions=0;
+      }
+      return attractions;
+    })
+  }
+
+  return (
+    <div className='App'>
+      {
+      landmark.length > 0 &&
+        <div>
+          <div className='appButton'>
+            <button className='buttonDel' onClick={() => delAttractions(landmark[attractions].id)}>Visited</button>
+          </div>
+          <div className='appAttractions'>
+            <h1>Explore Genoa's {landmark.length} Attractions</h1>
+          </div>
+          <div>
+            {currentImageIndex % 2 === 0 ? (
+            <img className='appImg' width='500px' src={landmark[attractions].imageOne} alt='pictures' />
+            ) : (
+            <img className='appImg' width='500px' src={landmark[attractions].imageTwo} alt='pictures' />
+            )}
+          </div>
+          <div className='appDescription'>
+            <h2>{landmark[attractions].name}</h2>
+          </div>
+          <div className='appDescription delDescription'>
+            <p>{landmark[attractions].description}</p>
+          </div>
+          <div className='appButtons'>
+            <button className='buttonPrev' onClick={previousAttractions}>Prev</button>
+            <button className='buttonNext' onClick={nextAttractions}>Next</button>
+          </div>
+        </div>
+      }
+   </div>
+  );
+};
+  
 export default App;
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+    id: 1,
+    imageOne: "https://www.artribune.com/wp-content/uploads/2017/08/genova.jpg",
+    imageTwo:"http://italia-ru.com/files/piazza_de_ferrari_a_genova8.jpg",
+    name: "Palazzo Ducale",
+    description: "Historical palace with beautiful artwork and stunning architecture."
+    },
+    {
+    id: 2,
+    imageOne: "https://img0.liveinternet.ru/images/attach/c/7/125/68/125068962_sunrise_in_genoa_italy_211.jpg",
+    imageTwo:"http://italia-ru.com/files/lighthouse_in_genova_italy6.jpg",
+    name: "The Lanterna",
+    description: "Iconic lighthouse offering panoramic views of the city and port."
+    },
+    {
+    id: 3,
+    imageOne: "https://lideamagazine.com/wp-content/uploads/2023/02/acquario-di-genova.jpg",
+    imageTwo:"https://italiabsolutely.com/media/k2/items/cache/274f1bf2bd18e7f0d33d6244e658426e_XL.jpg",
+    name: "Aquarium of Genoa",
+    description: "Largest aquarium in Italy with a variety of marine life exhibits."
+    },
+    {
+    id: 4,
+    imageOne: "https://fratiminoribeniartistici.it/wp-content/uploads/2019/03/06_Santissima-Annunziata-Genova.jpg",
+    imageTwo:"https://www.italyguide.info/pics/chiesa-annunziata-genova.jpg",
+    name: "Basilica of the Santissima Annunziata del Vastato",
+    description: "A beautiful example of Baroque, with stunning frescoes and a plethora of artistic works."
+    },
+    {
+    id: 5,
+    imageOne: "https://thumbs.dreamstime.com/b/monumental-cemetery-staglieno-genoa-italy-cimitero-monumentale-di-extensive-located-hillside-district-famous-153411694.jpg",
+    imageTwo:"https://finestresullarte.b-cdn.net/rivista/immagini/2022/1865/cimitero-di-staglieno-1.jpg",
+    name: "Staglieno Monumental Cemetery",
+    description: "Known for its marble mausoleums, sculptures, and architecture reflecting the rich history of Genoa."
+    }
+    ];
